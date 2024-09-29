7d43d35a83691636e3cc1eec1a3efa7f0c4f1373
masindes <maasinde.sylvester@gmail.com>
2024-09-29 21:03:42 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    const newArr = [];
+    for (let i = 0; i < array.length; i++) {
+      newArr.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return newArr;
+  }
+  function countDown(int) {
+    while (int > -1) {
+      console.log(int);
+      int--;
+    }
+  }
